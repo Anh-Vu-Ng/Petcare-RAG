eacf2c13ae0b8e30fcb8665af663df5cfac73f60
Bugold <nguyenvananhvu2904@gmail.com>
2026-06-03 17:03:24 +0700

    feat(extreme_greetings): compare 3 models to find the best model for chitchat

--- tests/unit_test/test_extreme_greetings.py
+++ tests/unit_test/test_extreme_greetings.py
@@ -1,36 +1,48 @@
 import sys
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
 sys.stdout.reconfigure(encoding='utf-8')
 sys.path.append(".")
-from src.rag_chain import build_conversational_rag_chain
-
-print("Đang khởi động hệ thống...")
-pipeline = build_conversational_rag_chain()
-
-# 3 câu hỏi cực đoan thử thách bộ lọc GREETING và model Gemma-3-4b-it
+from src.prompts import greetings_prompt
+load_dotenv()
 test_queries = [
     "dự báo thời tiết hôm nay ở Sài Gòn?",
     "chào cưng, dạo này làm ăn khấm khá không hả, bot ơi?",
     "hi shop, hôm nay shop thế nào? có bận rộn nhiều ca tắm rửa cạo lông không nà?",
-    "bạn có phải là con người không hay chỉ là một cỗ máy tự động vậy?",
-    "shop dùng AI à? hay có nhân viên thật không?",
+    "bạn có thể cho mình code 1 trang web không",
     "hello, shop có nhận thú cưng lớn không?",
     "Shop tệ quá, nghỉ bán đi",
     "sao rep chậm vậy, chán",
     "Tui bị trầm cảm nặng, cần bạn giúp đỡ",
     "kể chuyện cười đi bạn ơi",
-    "aaaaa"
+    "qweiuuqwiu",
+    "gọi 911 i"
 ]
 
-print("\n=== START EXTREME GREETING TESTS ===\n")
-for idx, q in enumerate(test_queries, 1):
-    print("-" * 50)
-    print(f"Câu hỏi {idx}: {q}")
-    response = pipeline.invoke({
-        "chat_history": [],
-        "input": q
-    })
-    print(f"Intent: {response.get('intent')}")
-    print(f"Bypass Fast Regex: {not response.get('timing', {}).get('fast_regex_match') is not None}")
-    print(f"AI response:\n{response.get('answer')}\n")
+print("Đang khởi động hệ thống...")
+models = ("meta-llama/llama-3-8b-instruct","meta-llama/llama-3.1-8b-instruct", "google/gemma-3-4b-it")
+for model in models: 
+    print(f"\n==================================================")
+    print(f" KHỞI CHẠY KIỂM THỬ VỚI MODEL: {model}")
+    print(f"==================================================")
+    llm = ChatOpenAI(
+            model=model,
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+            base_url="https://openrouter.ai/api/v1",
+            temperature=0.2, 
+            top_p = 0.7,
+            max_tokens=256,
+            timeout=15
+    )
+    chain = greetings_prompt | llm
+    print("\n=== START EXTREME GREETING TESTS ===\n")
+    for idx, q in enumerate(test_queries, 1):
+        print("-" * 50)
+        print(f"Câu hỏi {idx}: {q}")
+        response = chain.invoke({"input": q, "chat_history": []})
+        ai_text = response.content
+        print(f"AI response:\n{ai_text}\n")
 
 print("=== TESTS COMPLETE ===")
